996fb6fc95eb38e1dac86d76ffde4c8f519f0a8a
1nsomn1aa <serpenskas.tomas@gmail.com>
2025-04-07 20:30:46 +0100

    Create shop page layout and setup initial product filtering system

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Product
+from .models import Product, Category
 from .forms import ProductForm
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'shop/product_list.html', {'products': products})
+    categories = Category.objects.all()
+
+    category_filter = request.GET.getlist('category')
+    if category_filter:
+        products = products.filter(category__id__in=category_filter)
+
+    price_to = request.GET.get('price_to')
+    if price_to:
+        products = products.filter(price__lte=price_to)
+
+    return render(request, 'shop/product_list.html', {
+        'products': products,
+        'categories': categories,
+        'category_filter': category_filter
+    })
 
 def add_product(request):
     if request.method == 'POST':
@@ -33,3 +47,7 @@ def delete_product(request, pk):
         product.delete()
         return redirect('product_list')
     return render(request, 'shop/confirm_delete.html', {'product': product})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'shop/product_detail.html', {'product': product})
