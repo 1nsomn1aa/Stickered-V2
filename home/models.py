99bc174cac2b484d0d82a580656f4733868855b6
1nsomn1aa <serpenskas.tomas@gmail.com>
2025-04-15 13:52:36 +0100

    Add user testimonials section and setup functionality to store user testimonials

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+    role = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
