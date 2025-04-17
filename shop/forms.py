bbccbb0410d7862b7411e63f5047745c34e38222
1nsomn1aa <serpenskas.tomas@gmail.com>
2025-04-17 15:53:06 +0100

    Style checkout.html and make it responsive

--- shop/forms.py
+++ shop/forms.py
@@ -13,4 +13,29 @@ class ProductForm(forms.ModelForm):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'city', 'zip_code']
+        fields = ['first_name', 'last_name', 'email', 'address_line1', 'address_line2', 'city', 'eir_code', 'country']
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+            'address_line1': 'Street Address',
+            'address_line2': 'County',
+            'city': 'City / Town',
+            'eir_code': 'Eircode',
+            'country': 'Country',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Enter your first name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Enter your last name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email address'}),
+            'address_line1': forms.TextInput(attrs={'placeholder': 'Street address'}),
+            'address_line2': forms.TextInput(attrs={'placeholder': 'County'}),
+            'city': forms.TextInput(attrs={'placeholder': 'City or Town'}),
+            'eir_code': forms.TextInput(attrs={'placeholder': 'Eircode or Postal Code'}),
+            'country': forms.Select(choices=[('Ireland', 'Ireland')]),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
