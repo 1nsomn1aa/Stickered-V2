7404aa480efe3c6b9103c785fee72c3cf64ccddf
1nsomn1aa <serpenskas.tomas@gmail.com>
2025-04-25 19:27:42 +0100

    Replace database settings to use Heroku Postgres DB

--- stickered/settings.py
+++ stickered/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from storages.backends.s3boto3 import S3Boto3Storage
 from pathlib import Path
 from dotenv import load_dotenv
@@ -107,16 +108,8 @@ WSGI_APPLICATION = 'stickered.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', default='5432'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
