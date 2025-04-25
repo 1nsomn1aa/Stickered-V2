63919c50a3ee648ad2ad498dc4b784be93b08dd5
1nsomn1aa <serpenskas.tomas@gmail.com>
2025-04-25 16:34:43 +0100

    Create Procfile, setup Allowed Hosts, hide secrets with environment variables, turn off Debug mode

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stickered.wsgi --log-file -
--- stickered/settings.py
+++ stickered/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fmy2jst@7&j2q$15b5!h-iq6n_on=k^$*+!gevvh8jj6$i62b-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://stickered-9f7c00b2095b.herokuapp.com/']
 
 
 # Application definition
