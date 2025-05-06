7fa72214844ed2cc290ec88d533e314a25263042
1nsomn1aa <serpenskas.tomas@gmail.com>
2025-05-06 14:36:21 +0100

    Create a new Django Secret Key and put it in environment variables

--- stickered/settings.py
+++ stickered/settings.py
@@ -49,7 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fmy2jst@7&j2q$15b5!h-iq6n_on=k^$*+!gevvh8jj6$i62b-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
